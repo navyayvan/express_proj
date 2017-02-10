6a317c89f990372a088a4137e24a7c8dcf48f0a0
navyayvan <navyayvan@gmail.com>
2017-02-09 20:50:35 -0800

    created schema, ran migration, began API

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var app = express();
 var layouts = require('express-ejs-layouts');
+var db = require('./models');
 
 app.set('view engine', 'ejs');
 
@@ -14,19 +15,37 @@ app.get('/', function(req,res) {
 });
 
 app.get('/articles', function(req,res) {
-  res.render('articles/index.ejs', {taco: 'veggie tacos'});
+  db.article.findAll().then(function(articles) {
+    res.render('articles/index.ejs', {title: title, body: body});
+  })
+  
 })
 
 app.get('/articles/new', function(req,res) {
   res.render('articles/new.ejs');
 })
 
-app.post('/articles', function(req,res) {
-  res.send(req.body);
+app.post('/articles/new', function(req,res) {
+  var title = req.body.title;
+  var body = req.body.body;
+
+  db.article.create(
+    { title: article.title, body: article.body}
+  ).then( function() {
+    res.redirect('/articles');
+  })
+});
+
+app.get('/articles/:id', function(req,res) {
+  res.send();
+});
+
+app.get('/about', function(req,res) {
+  res.render('sites/about.ejs');
 })
 
-app.get('/greetings/:name/:lastname', function(req,res) {
-  res.send(req.params.name + " " + req.params.lastname);
-});
+app.get('/contact', function(req,res) {
+  res.render('sites/contact.ejs');
+})
 
 app.listen(3000);
